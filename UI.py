f14b53badd4ff31370aacc82ff5cae0199744355
Jon2089 <abdurrehman.otaku@gmail.com>
2024-07-16 22:29:28 +0500

    app complete

--- UI.py
+++ UI.py
@@ -3,14 +3,23 @@ import plotly.express as px
 from backend import get_data
 
 st.title("Weather Forecast for the Next Days")
-place = st.text_input("PLace: ")
+place = st.text_input("Place: ")
 days = st.slider("Forecast Days", min_value=1, max_value=5,
                  help="Select the number of forecasted days")
-option = st.selectbox("Select the date to view", ("Temperature", "Sky"))
+option = st.selectbox("Select the data to view", ("Temperature", "Sky"))
 
 st.subheader(f"{option} for the next {days} days in {place}")
 
-d, t = get_data((place, days, option))
+if place:
+    try:
+        data, times = get_data(place, days, option)
+        if option == "Temperature":
+            data = [t / 10 for t in data]
+            figure = px.line(x=times, y=data, labels={"x": "Date", "y": "Temperature (C)"})
+            st.plotly_chart(figure)
 
-figure = px.line(x=d, y=t, labels={"x": "Date", "y": "Temperature (C)"})
-st.plotly_chart(figure)
+        if option == "Sky":
+            image_paths = [f"images/{weather.lower()}" + ".png" for weather in data]
+            st.image(image_paths, width=115, caption=times)
+    except KeyError:
+        st.info("Please enter a real city!")
--- backend.py
+++ backend.py
@@ -2,16 +2,17 @@ import requests
 
 API_KEY = "d522e70e36a474ed58047847e1f917b1"
 
-def get_data(place, forecast_days=None, kind=None):
+def get_data(place=None, forecast_days=1, kind="Temperature"):
     url = f"https://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_KEY}"
     response = requests.get(url)
     data = response.json()
     nr_values = 8 * forecast_days
+    times = [data['list'][i]['dt_txt'] for i in range(nr_values)]
     if kind == "Temperature":
         filtered_data = [data['list'][i]['main']['temp'] for i in range(nr_values)]
     if kind == "Sky":
         filtered_data = [data['list'][i]['weather'][0]['main'] for i in range(nr_values)]
-    return filtered_data
+    return filtered_data, times
 
 if __name__ == "__main__":
     print(get_data(place='lahore', forecast_days=3, kind="Sky"))
